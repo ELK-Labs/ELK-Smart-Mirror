ef2c22773c516d87360b3237f3d59a9f4b32a701
Peter McKinney <pmck91@gmail.com>
2016-03-19 00:34:16 +0000

    added correct keys.js file

---
+++ src/js/keys.js
@@ -0,0 +1,10 @@
+//! keys
+//! Author : Peter McKinney
+//! On : 19/03/2016
+//! Licence : MIT
+//! elklabs.io
+
+export const keys = {
+    quote: "YOUR MASHAPE KEY",
+    weather: "YOUR OPEN WEATHER KEY"
+}
