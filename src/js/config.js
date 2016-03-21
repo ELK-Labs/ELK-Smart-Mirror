8dc7e0c9e6b9a17e6882ad1f1b582c7c0d466b41
Peter McKinney <devteam@elklabs.io>
2016-03-21 17:41:13 +0000

    added some instructions

--- src/js/config.js
+++ src/js/config.js
@@ -8,79 +8,103 @@ import { keys } from "./keys";
 
 export const config = {
 
+    /* Some Common Times in ms
+     * 1 second : 1000
+     * 1 minute : 60000
+     * 5 minutes : 300000
+     * 15 minutes : 900000
+     * 30 minutes : 1800000
+     * 1 hour : 3600000
+     */
+
+    // view locales here http://momentjs.com/#multiple-locale-support
     Locale: 'en-ie',
 
-    CORSProxy: 'https://crossorigin.me/',
+    //if you are having trouble with cross origin errors, use one of the following (1st one is best) & set proxy to true
+    //CORSProxy: 'https://crossorigin.me/',
     //CORSProxy: 'http://cors.io/?u=',
 
-    Time: {
-        timeLoc: '.time',
-        dateLoc: '.date',
-        secondClass: '.second',
-        updateInterval: 1000
+    Proxy: {
+        use: false,
+        url: 'https://crossorigin.me/'
     },
+
+    // the fade out + in duration, set to 1 to disable NOT 0 (computers hate dividing by zero)
+    animationDuration: 1000,
+
+    Time: {
+        timeLoc: 'time',
+        dateLoc: 'date',
+        updateInterval: 1000, // how often the time will refresh
+        format: 'h:mm:ss A' // view formatting info here http://momentjs.com/docs/#/parsing/string-format/
+    },
+
     News: {
+        // add rss news feeds here
         feeds: [
             'http://waterfordwhispersnews.com/category/breaking-news/feed/',
             'http://www.theonion.com/feeds/rss'
         ],
         location: 'news',
-        updateInterval: 1800000, // 30 minutes
-        showInterval: 60000,
-        maxNewsItemsPerFeed: 25,
-        maxDisplayItems: 5
+        updateInterval: 3600000, // how often to pull new news stories
+        showInterval: 60000, // how often to scroll the headlines
+        maxNewsItemsPerFeed: 25, // how many items per each feed will be displayed
+        maxDisplayItems: 5 // how many items will be displayed at once
     },
 
     Weather: {
         apiKey: keys.weather,
         apiBase: "https://api.forecast.io/forecast",
-        lat: 53.3478,
+        lat: 53.3478, // find your lat and long on google maps
         long: -6.2597,
         useMetric: true,
-        useKnots: false,
+        useKnots: false, // use knots for wind speed
         feelsLikeLoc: "feelslike",
         summaryLoc: "weathersummary",
         currentLoc: "temp",
         windLoc: "wind",
         sunLoc: "sun",
         forecastLoc: "forecast",
-        updateInterval: 3600000
+        updateInterval: 3600000 // how ofter the forecast updates
     },
 
     Message: {
         messageLocation: "message",
-        useQuotes: false,
-        updateInterval: 3600000,
+        useQuote: false,
+        updateInterval: 1800000, // how often the quotes/messages update
         quote: {
             apiEndpoint: "https://andruxnet-random-famous-quotes.p.mashape.com/",
             apiKey: keys.quote
         },
         messages: {
-            morning: [
+            morning: [ // 6AM to 12PM
                 "Good Morning!",
                 "Have a Great Day!",
                 "Looking Well!",
                 "Don't Forget a Coffee!",
                 "Have a Nice Sleep?"
             ],
-            evening: [
+            evening: [ // 12PM to 6PM
                 "Enjoy Your Lunch!",
                 "Hope The Day is Going Well!",
                 "Jaysus, What a Stunner!",
                 "Fine Day for it!"
             ],
-            night: [
+            night: [ // 6PM to 12AM
                 "Have a Great Night!",
                 "Sweet Dreams!",
                 "Early to Bed, Early to Rise"
             ],
-            extremeNight: [
+            lateNight: [ // 12AM to 6AM
                 "Shouldn't You Be Asleep...",
                 "Sleep, who has time for it?",
                 "Get to Bed!"
             ]
-        }
-    },
-
+        },
+        birthdays: [ // dates are in standard format (not us) so DD/MM
+            ["First Birthday Name", "01/04"],
+            ["Second Birthday Name", "23/03"]
+        ]
+    }
 
 };
